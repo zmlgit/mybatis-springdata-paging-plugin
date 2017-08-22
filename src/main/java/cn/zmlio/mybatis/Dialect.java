ee9d95c8ccf17a7910c292bdb90d05cd7ce9fd70
zmlgit <zmanliang@163.com>
2017-08-22 12:03:35 +0800

    优化count语句

--- src/main/java/cn/zmlio/mybatis/Dialect.java
+++ src/main/java/cn/zmlio/mybatis/Dialect.java
@@ -3,11 +3,24 @@ package cn.zmlio.mybatis;
 public abstract class Dialect {
 
     public String buildCountSQL(String originSQL) {
-        String countSQL = "SELECT count(1) from (" + originSQL + ") as a";
-        return countSQL;
+
+        if (isUnionQuery(originSQL)) {
+            return "SELECT count(1) from (" + originSQL + ") as a";
+        } else {
+            int selectIndex = originSQL.toLowerCase().indexOf("select");
+            int fromIndex = originSQL.toLowerCase().indexOf("from");
+            String countSQL = originSQL.substring(selectIndex, selectIndex + "select".length());
+            countSQL += " count(1) ";
+            countSQL += originSQL.substring(fromIndex);
+            return countSQL;
+        }
     }
 
     public abstract String buildPageSQL(String originSQL, int offset, int limit);
+
+    protected boolean isUnionQuery(String originSql) {// 使用union的语句count无法优化
+        return originSql.toLowerCase().contains("union");
+    }
 
     public boolean supportPaging() {
         return true;
